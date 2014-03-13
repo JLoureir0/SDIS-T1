ae33b5570c99f0e552a7bbd72ae59914e706bf6d
Daniel Moreira <danielmoreira1993@gmail.com>
2014-03-13 15:32:43 +0000

    Adicionado ChunkDeleteClient

---
+++ src/subprotocols/ChunkDeleteClient.java
@@ -0,0 +1,43 @@
+package subprotocols;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class ChunkDeleteClient {
+	
+	private static String fileId;
+	private static int MCPort;
+	private static InetAddress mcAddress;
+	private static DatagramPacket sendPacket;
+	private static DatagramSocket socket;
+	private static int numberOfDeleteMessages;
+
+	public static void main(String[] args) {
+		try {
+			socket = new DatagramSocket();
+			
+			for(int i=0;i<numberOfDeleteMessages;i++) {
+				String request = createPacket();
+				sendPacket(request);
+			}
+
+		}catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public static String createPacket() {
+		String request = "DELETE";
+		request = " "+fileId+" CRLF CRLF"; 
+		return request;
+	}
+	
+	public static void sendPacket(String request) throws Exception {
+		byte[] asciiRequest = request.getBytes("US-ASCII");
+		sendPacket = new DatagramPacket(asciiRequest,asciiRequest.length,mcAddress,MCPort);
+		socket.send(sendPacket);
+		return;
+	}
+	
+}
