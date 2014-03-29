f01db878f5f98da6782e2f701ac3a1fba3f8852e
Daniel Moreira <danielmoreira1993@gmail.com>
2014-03-29 12:34:04 +0000

    Nao estou a conseguir criar as duas bases de dados

--- src/ui/Ui.java
+++ src/ui/Ui.java
@@ -1,17 +1,53 @@
 package ui;
 
+import java.net.InetAddress;
 import java.util.Scanner;
 
+import backingup.FileManager;
+import backingup.ipeer.action.FileBackup;
+import backingup.ipeer.database.Database;
+import backingup.peer.database.Database;
+
 public class Ui {
+	
+	private static backingup.ipeer.database.Database ipeerDb;
+	private static backingup.peer.database.Database peerDb;
+	
+	private static int mcPort;
+	private static String address;
+	private static int mdbPort;
+	private static String address1;
+	private static int mdrPort;
+	private static String address2;
+	
+	private static InetAddress mcAddress;
+	private static InetAddress mdbAddress;
+	private static InetAddress mdrAddress;
 
     public static void main(String[] args) {
+    	setAdresses();
     	//Create listeners
     	//Create SearchDeletedFiles Thread
     	//Portas ips dbsize
         mainMenu();
     }
     
-    public static void mainMenu() {
+    private static void setAdresses() {
+    	try {
+    		mcAddress = InetAddress.getByName(address);
+    		mdbAddress = InetAddress.getByName(address1);
+    		mdrAddress = InetAddress.getByName(address2);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+    private static void initializaDatabases(int dbMaxSize) {
+    	ipeerDb = new Database();
+    	peerDb = new Database(dbMaxSize);
+    }
+    
+    private static void mainMenu() {
     	while(true) {
     		printMainMenu();
     		getMainMenuChoice();
@@ -50,6 +86,40 @@ public class Ui {
         		 Thread.sleep(500);
                  break;
     	}
+    }
+    
+    private static void backupFile() {	
+    	String path = "";
+    	int replicationDegree = 0;
+    	while(true) {
+        	Scanner keyboard = new Scanner(System.in);
+        	System.out.println("Please insert the path of the file you want to backup: ");
+        	path = keyboard.next();
+        	
+        	boolean fileExists = false;
+        	FileManager fm = new FileManager(path);
+        	fileExists = fm.CheckIfFileExists();
+        	if(fileExists) 
+        		break;
+        	System.out.println("Error, incorrect file path!");
+    	}
+    	
+    	while(true) {
+        	Scanner keyboard = new Scanner(System.in);
+        	System.out.print("Please insert the replication degree of the file: ");
+        	replicationDegree = keyboard.nextInt();
+        	
+        	if(replicationDegree > 0)
+        		break;
+        	System.out.println("Error, invalid file replication degree!");
+    	}
+    
+    	FileBackup fb = new FileBackup(path, replicationDegree, db, mdbPort, mdbAddress, mcPort, mcAddress);
+    	boolean result = fb.backupFile();
+    	if(result)
+    		System.out.println("The file has been backup up sucessfully!");
+    	else
+    		System.out.println("It was not possible to backup the desired file, pleasy try again!");
     }
     
     
