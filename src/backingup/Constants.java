6ec05370244dd5ddcead88aa2930a9948f9bc651
João Loureiro <joao.a.g.loureiro@gmail.com>
2014-03-29 15:10:56 +0000

    Updated Constants

--- src/backingup/Constants.java
+++ src/backingup/Constants.java
@@ -4,15 +4,15 @@ import java.io.File;
 
 public class Constants {
 	public static final int ARRAY_SIZE = 132096;
-	public static int CHUNKSIZE = 65536;
+	public static final int CHUNKSIZE = 65536;
 	public static final int HALF_A_SECOND = 500;
 	public static final int SLEEP = 401;
 	public static final int NUMBER_OF_DELETED_MESSAGES = 5;
 	public static final String PUTCHUNK = "PUTCHUNK";
 	public static final String STORED = "STORED";
-	public static String GETCHUNK = "GETCHUNK";
+	public static final String GETCHUNK = "GETCHUNK";
 	public static final String CHUNK = "CHUNK";
-	public static String DELETE = "DELETE";
+	public static final String DELETE = "DELETE";
 	public static final String REMOVED = "REMOVED";
 	public static final String VERSION_1 = "1.0";
 	public static final String CRLF = "CRLFCRLF";
