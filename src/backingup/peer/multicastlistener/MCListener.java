072bb0ada21eec6d1db3e779f6424c48c77c232d
João Loureiro <joao.a.g.loureiro@gmail.com>
2014-03-29 20:59:42 +0000

    Fixed bugs of MCListener

--- src/backingup/peer/multicastlistener/MCListener.java
+++ src/backingup/peer/multicastlistener/MCListener.java
@@ -53,15 +53,15 @@ public class MCListener extends Thread {
 	
 	private void parseChunk(String chunk) {
 		String[] chunkSplit = chunk.split(Constants.WHITESPACE_REGEX);
-		if(chunkSplit[0].equals(Constants.GETCHUNK) && (chunkSplit.length == 5)) {
+		if(chunkSplit[0].equals(Constants.GETCHUNK)) {
 			ChunkRestore chunkRestore = new ChunkRestore(database, chunkSplit[2], Integer.parseInt(chunkSplit[3]), mdrPort, mdrAddress, mcPort, mcAddress);
 			chunkRestore.start();
 		}
-		else if(chunkSplit[0].equals(Constants.DELETE) && (chunkSplit.length == 3)) {
+		else if(chunkSplit[0].equals(Constants.DELETE)) {
 			ChunkDelete chunkDelete = new ChunkDelete(database, chunkSplit[2]);
 			chunkDelete.start();
 		}
-		else if(chunkSplit[0].equals(Constants.REMOVED) && (chunkSplit.length == 5)) {
+		else if(chunkSplit[0].equals(Constants.REMOVED)) {
 			ChunkRemoved chunkRemoved = new ChunkRemoved(database, chunkSplit[2], Integer.parseInt(chunkSplit[3]), Integer.parseInt(chunkSplit[4]), mdbPort, mdbAddress, mcPort, mcAddress);
 			chunkRemoved.start();
 		}
